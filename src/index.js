75c014c1e5b6af68b8c7c263428a56cbd849fd56
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-05-26 17:41:36 +0300

    php

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const btnLoader = document.querySelector('[data-action="loader"]');
 
 form.addEventListener('submit', formSend);
 
-function formSend(e) {
+async function formSend(e) {
   e.preventDefault();
 
   btn.classList.remove('btn');
@@ -16,15 +16,21 @@ function formSend(e) {
   btnLoader.classList.add('btn-loader');
 
   let formData = new FormData(form);
-  let response = axios.post('sendmail.php', {
+  let response = await fetch('sendmail.php', {
     method: 'POST',
     body: formData,
   });
   if (response.ok) {
-    let result = response.json();
+    let result = await response.json();
     alert(result.message);
     formPreview.innerHTML = '';
     form.reset();
+    btnLoader.classList.remove('btn-loader');
+    btnLoader.classList.add('btn-closed');
+    btn.classList.remove('btn-closed');
+    btn.classList.add('btn');
+  } else {
+    alert('Ошибка');
     btnLoader.classList.remove('btn-loader');
     btnLoader.classList.add('btn-closed');
     btn.classList.remove('btn-closed');

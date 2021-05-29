749afbbee2642495d2c9906b921240a46d2119e3
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-05-29 13:17:30 +0300

    add modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,16 @@
 const openModalBtn = document.querySelector('button[data-action="open-modal"]');
-const closeModalBtn = document.querySelector('menu[data-action="open-modal"]');
+const closeModalBtn = document.querySelector('button[data-action="close-modal"]');
+document.querySelectorAll('.js-click').forEach(closeLink => {
+  closeLink.addEventListener('click', () => {
+    document.body.classList.remove('show-modal');
+  });
+});
 openModalBtn.addEventListener('click', () => {
   document.body.classList.add('show-modal');
+});
+
+closeModalBtn.addEventListener('click', () => {
+  document.body.classList.remove('show-modal');
 });
 
 export { openModalBtn };
